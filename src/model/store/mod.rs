cd8d56239e0c1965e9649320fe60fdbd32c943d7
sjava <515957966@qq.com>
2023-12-20 08:59:49 +0800

    update max_connection

--- src/model/store/mod.rs
+++ src/model/store/mod.rs
@@ -10,7 +10,7 @@ pub type Db = Pool<Postgres>;
 
 pub async fn new_db_pool() -> Result<Db> {
 	PgPoolOptions::new()
-		.max_connections(5)
+		.max_connections(1)
 		.connect(&config().DB_URL)
 		.await
 		.map_err(|e| Error::FailToCreatePool(e.to_string()))
