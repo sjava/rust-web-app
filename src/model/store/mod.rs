a0e60a416d32aacea40c5b3b2fee514133ba26b1
sjava <515957966@qq.com>
2023-12-20 09:21:03 +0800

    set max_connections=1 for test

--- src/model/store/mod.rs
+++ src/model/store/mod.rs
@@ -11,8 +11,10 @@ use sqlx::{Pool, Postgres};
 pub type Db = Pool<Postgres>;
 
 pub async fn new_db_pool() -> Result<Db> {
+	let max_connections = if cfg!(test) { 1 } else { 5 };
+
 	PgPoolOptions::new()
-		.max_connections(1)
+		.max_connections(max_connections)
 		.connect(&config().DB_URL)
 		.await
 		.map_err(|e| Error::FailToCreatePool(e.to_string()))
